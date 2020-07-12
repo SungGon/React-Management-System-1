47925ae8ee773c5043648a307e30c1851437494c
SungGon <gskon@hanmail.net>
2020-07-13 02:16:44 +0900

    multiple people

--- src/App.js
+++ src/App.js
@@ -1,13 +1,50 @@
 import React, {Component} from 'react';
-import logo from './logo.svg';
+import Customer from './components/Customer'
 import './App.css';
+
+
+const customer=[
+{
+  'id':1,
+  'image':'http://placeimg.com/64/64/1',
+  'name':'홍길동',
+  'birthday':'961222',
+  'gender':'남자',
+  'job':'대학생'
+},
+{
+  'id':2,
+  'image':'http://placeimg.com/64/64/2',
+  'name':'박지호',
+  'birthday':'941121',
+  'gender':'남자',
+  'job':'대학생'
+},
+{
+  'id':3,
+  'image':'http://placeimg.com/64/64/3',
+  'name':'염현교',
+  'birthday':'940521',
+  'gender':'남자',
+  'job':'대학생'
+}
+]
+
 
 class App extends Component{
   render(){
     return(
-      <div className="gray-background">
-        <img src={logo} alt="logo"/>
-        <h2>Let's develop management system!</h2>
+      <div>
+        {customer.map(c=>{
+          return <Customer  key={c.id} 
+                            id={c.id}
+                            image={c.image}
+                            name={c.name}
+                            birthday={c.birthday}
+                            gender={c.gender}
+                            job={c.job}
+                    />
+        })}
       </div>
     );
   }
